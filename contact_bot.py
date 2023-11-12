4329d173036e2c0af532554ac38181be4f72cabc
ivan1melnyk <139176450+ivan1melnyk@users.noreply.github.com>
2023-11-12 23:08:40 +0200

    input_error
    only one decoraitor
    inpyt_error

--- contact_bot.py
+++ contact_bot.py
@@ -6,30 +6,18 @@ def input_error(func):
     def inner(user_input):
         user_input = user_input.split(' ')
         try:
-            result = func(user_input)
+            return func(user_input)
         except KeyError:
             print("Give me name and phone please")
         except ValueError:
             print("Give me name and phone please")
         except IndexError:
             print("Give me name and phone please")
-        return result
     return inner
 
 
-def input_error_one(func):
-    def inner(user_input):
-        user_input = user_input.split(' ')
-        try:
-            result = func(user_input)
-        except KeyError:
-            print("Enter user name")
-        except ValueError:
-            print("Enter user name")
-        except IndexError:
-            print("Enter user name")
-        return result
-    return inner
+def hello():
+    print("How can I help you?")
 
 
 @input_error
@@ -46,18 +34,33 @@ def change(user_input):
     print('CHANGE USERS_CONTACTS:', user_input[1], 'NUMER:', user_input[2])
 
 
-@input_error_one
+@input_error
 def phone(user_input):
     try:
         print(USERS_CONTACTS[user_input[1]])
-    except:
+    except IndexError:
+        print('Enter user name')
+    except KeyError:
         print('This contact doesn\'t exist')
+
+
+def show_all():
+    if USERS_CONTACTS == {}:
+        print('Your contacts list is empty')
+    for name, number in USERS_CONTACTS.items():
+        # print(f'Name:{contact[0]}','Number:'{contact[1]})
+        print('{:^5} {:^10} {:^7} {:^10}'.format(
+            'Name:', name, 'Number:', number))
+
+
+def bye_bye():
+    print("Good bye!")
 
 
 while True:
     user_input = input()
     if user_input == "hello":
-        print("How can I help you?")
+        hello()
     elif user_input.startswith("add"):
         add(user_input)
     elif user_input.startswith("change"):
@@ -65,8 +68,14 @@ while True:
     elif user_input.startswith("phone"):
         phone(user_input)
     elif user_input == "show all":
-        print(USERS_CONTACTS)
+        show_all()
     elif user_input == "good bye" or user_input == "close" or user_input == "exit":
-        print("Good bye!")
+        bye_bye()
     elif user_input == '.':
         break
+    else:
+        print('//Error')
+        print('//Unknown comand')
+
+
+# print мають бути в функції main, тільки в ній
